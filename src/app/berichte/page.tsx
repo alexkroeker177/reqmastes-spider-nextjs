a36b5d4d2ba70439003f191e4cd1fb76d675c48a
Alexander Kröker <80253858+alexkroeker177@users.noreply.github.com>
2025-03-11 11:04:44 +0100

    try different component import

--- src/app/berichte/page.tsx
+++ src/app/berichte/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import * as React from "react";
-import { Card, CardContent, CardHeader, CardTitle, CardDescription } from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle, CardDescription } from "../../components/ui/card";
 import { DatePickerWithRange } from "@/components/ui/datepickerwithrange"
 import { FancyMultiSelect } from "@/components/ui/multiselect"
 import { Button } from "@/components/ui/button"
